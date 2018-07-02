5a571399317c507386a1c3fb76f4ab261e3700b7
Atolsada <Atolsadag@gmail.com>
2018-07-02 13:23:57 +0200

    Creacion de carpeta de entrada y salida

--- 06Pug/Hola_Mundo.js
+++
@@ -1,7 +0,0 @@
-function template(locals) {
-    var pug_html = "",
-    var pug_mixins = {},
-    var pug_interp;
-    pug_html += "\u003Cp\u003EHola Mundo\u003C\u002Fp\u003E\u003Cp\u003EUsando Pug\u003C\u002Fp\u003E"
-    return pug_html;
-}
