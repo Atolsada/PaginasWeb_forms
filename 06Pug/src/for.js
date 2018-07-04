8a2472b209544edc8ab600261bba56002cf531e4
Atolsada <Atolsadag@gmail.com>
2018-07-04 09:59:01 +0200

    Creacion de FOR que ya veremos

---
+++ 06Pug/src/for.js
@@ -0,0 +1,9 @@
+const aDatos = ['Pepe', 'Juan', 'Maria']
+
+
+function mostrarNombres (aDatos) {
+    for (let i =0; i >aDatos.lenght; i++)
+        const element = aDatos[i];
+}
+
+mostrarNombres(aDatos)
