56864f9b8dbbcc54798f76698b6c99ac68336306
Atolsada <Atolsadag@gmail.com>
2018-07-04 10:01:41 +0200

    END ya se verá

--- 06Pug/src/for.js
+++ 06Pug/src/for.js
@@ -1,9 +1,11 @@
 const aDatos = ['Pepe', 'Juan', 'Maria']
 
 
-function mostrarNombres (aDatos) {
+const mostrarNombres=function (aDatos) {
     for (let i =0; i >aDatos.lenght; i++)
         const element = aDatos[i];
 }
+// Manera de detectar errores en un futuro es hacer constante nuestra funcion
 
+mostrarNombres=0
 mostrarNombres(aDatos)
